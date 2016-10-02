6cc8dead6a3235c930eca8ffdcd5e99dc68e9704
angelicahl88 <angelica@geoshepherds.com>
2016-10-02 11:47:18 +0200

    created initial project configuration setup plus basic structure for client scripts

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/app.scss';
+
+render(<p>Hello there toastie</p>, document.getElementById('root'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'client')
+      },
+      {
+        test: /\.scss$/,
+        include: path.join(__dirname, 'client'),
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
